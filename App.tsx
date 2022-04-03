3ad9c89f3aafa5c92d5e04da61f0ddd7c5e1dc41
Vadim Kisilov <49349216+Kisilov-Vadim@users.noreply.github.com>
2022-04-03 18:40:18 +0300

    add eslint + prettier + react navigation

--- App.tsx
+++ App.tsx
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
